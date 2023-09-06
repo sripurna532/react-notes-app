6f93f51f0b815f5252d7d0c2d03a520f936d9d61
Sripurna Maitra Biswas <76967321+sripurna532@users.noreply.github.com>
2023-09-06 11:34:46 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+import axios from "axios";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+  // React.useEffect(() => {
+  //   axios.get(localhost3000).then((response) => {
+  //     setPost(response.data);
+  //   });
+  // }, []);
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
